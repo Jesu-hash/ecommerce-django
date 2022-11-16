2ada008d61b476d25a84770a0c439db042b64ffb
jprieto <jesusprieto701@hotmail.com>
2022-11-15 21:46:36 -0300

    enviar codigo con ur permitida

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['ecommerce-env.eba-wmwtjmkz.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 #ALLOWED_HOSTS = []
 
 # Application definition
