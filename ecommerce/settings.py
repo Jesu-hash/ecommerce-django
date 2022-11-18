31de8660db1726d8cd108bc85e8a716762cac050
jprieto <jesusprieto701@hotmail.com>
2022-11-18 13:34:40 -0300

    update script with commands to migrate db

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['ecommerce-env.eba-iramsmnm.us-west-2.elasticbeanstalk']
+ALLOWED_HOSTS = ['ecommerce-env.eba-iramsmnm.us-west-2.elasticbeanstalk.com']
         
 #ALLOWED_HOSTS = []
 
