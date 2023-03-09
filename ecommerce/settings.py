cb281dd3b17bbf32f995afddc46e10d742483a54
jprieto <jesusprieto701@hotmail.com>
2023-03-08 21:52:37 -0300

    add image configuration AWS

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from decouple import config
 from django.utils.translation import gettext_lazy as _
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['ecommerce-env.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['ecommerce-env.us-west-2.elasticbeanstalk.com', '*']
         
 #ALLOWED_HOSTS = []
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'carts',
     'orders',
     'admin_honeypot',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -96,7 +97,7 @@ AUTH_USER_MODEL = 'accounts.Account'
 #             'ENGINE': 'django.db.backends.postgresql',
 #             'NAME': 'ebdb',
 #             'USER': 'jprieto',
-#             'PASSWORD': 'Lochita22',
+#             'PASSWORD': 'sardina12',
 #             'HOST': 'awseb-e-kwgunipqxm-stack-awsebrdsdatabase-ub8wtajqm58s.cofzpy4gowww.us-west-2.rds.amazonaws.com',
 #             'PORT': 5432,
 #         }
@@ -163,13 +164,34 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR /'static'
-#STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+# STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR /'static'
+# #STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+
+# STATICFILES_DIRS = [
+#     'ecommerce/static'
+# ]
+
+# AWS S3 Static Files Configuration
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
-    'ecommerce/static'
+    'ecommerce/static',
 ]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'ecommerce.media_storages.MediaStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 
